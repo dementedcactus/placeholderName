c7bd7bff680e4caa40221fcc7b726cc846b3f5d0
Lisa <imnotlostrui@aim.com>
2018-03-20 12:43:48 -0400

    Add createEvent VC, eventdETAIL VC

--- whosfree/VCs/CreateEventViewController.swift
+++ whosfree/VCs/CreateEventViewController.swift
@@ -10,26 +10,28 @@ import UIKit
 
 class CreateEventViewController: UIViewController {
 
+    let createEventView = CreateEventView()
+    var dummyData = ["test1 title", "test2 title", "test3 title"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.addSubview(createEventView)
+        self.createEventView.tableView.dataSource = self
+        self.createEventView.tableView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+}
+extension CreateEventViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyData.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = dummyData[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventTypeCell", for: indexPath)
+        cell.textLabel?.text = data
+        return cell
+    }
+    
 
 }
